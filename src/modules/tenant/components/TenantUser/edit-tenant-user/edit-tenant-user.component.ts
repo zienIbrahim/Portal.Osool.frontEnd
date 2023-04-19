011c697e5c0c21afa00b3317d6a96d17c66539d3
zienIbrahim <zainal3abden@gmail.com>
2023-04-20 02:51:06 +0300

    []

--- src/modules/tenant/components/TenantUser/edit-tenant-user/edit-tenant-user.component.ts
+++ src/modules/tenant/components/TenantUser/edit-tenant-user/edit-tenant-user.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatIconButton } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { NotificationService } from 'src/modules/app-common/services/notification.service';
 import { TenantList } from 'src/modules/tenant/data/Tenant';
+import { UserData } from 'src/modules/tenant/data/TenantUser';
 import { TenantUserService } from 'src/modules/tenant/services/tenant-user.service';
 
 @Component({
@@ -14,6 +15,7 @@ import { TenantUserService } from 'src/modules/tenant/services/tenant-user.servi
 export class EditTenantUserComponent implements OnInit{
   TenantUserform: FormGroup=<FormGroup>{};
   TenantList:TenantList[]=[]
+  UserData:UserData=<UserData>{};
   @Input()  TenantUserId: string='';
 
   constructor(private formBuilder: FormBuilder,
@@ -25,28 +27,34 @@ export class EditTenantUserComponent implements OnInit{
     this._GetUserTenantById()   
     this._getMAsterData()   
    }
+
    _GetUserTenantById(){
     if( this.TenantUserId!=''){
       this.tenantUserService.GetUserTenantById(this.TenantUserId).subscribe({
         next: (value: any) => {
-        console.log("value -> GetUserTenantById",value)   
+          this.UserData=value
+          this._setFormValue()   
      }});
     }
    
    }
-intiForm(){
+
+  intiForm(){
     this.TenantUserform = this.formBuilder.group({
       UserId: [null, [Validators.required]],
       userName: [null, [Validators.required]],
       email: [null, [Validators.required]],
       phoneNumber: [null, [Validators.required]],
-      password: [null, [Validators.required]],
-      ConfiremPassword: [null, [Validators.required]],
       tenantList: [null, [Validators.required]],
+      isActive: [false, [Validators.required]],
+      userInGroups: this.formBuilder.array([]),
+
     });
   }
+
   onSubmit(addTemplateClose:MatIconButton){
   }
+
   _getMAsterData(){
     this.tenantUserService.GetAllTenant().subscribe({
       next:(value:any)=> {
@@ -62,7 +70,50 @@ intiForm(){
     }
     ) 
   }
+  adduserInGroup(): void {
+    this.userInGroupsList.push(
+      this.formBuilder.group({
+        groupAdmin:['', Validators.required],
+        isActive:['', Validators.required],
+        tenantId:['', Validators.required],
+        tenantUserId:['', Validators.required],
+        timeAdded:['', Validators.required],
+        tenantName:['', Validators.required],
+        databaseName:['', Validators.required],
+        tenantinsertTs:['', Validators.required],
+        tenantGroupType:['', Validators.required],
+        tenantGroupTypeId:['', Validators.required],
+        tenantGroupTypeName:['', Validators.required],
+        timeRemoved:['', Validators.required]
+      })
+    );
+  }
+  _setFormValue(){
+    this.TenantUserform.patchValue({
+      UserId: this.UserData.userId,
+      userName: this.UserData.userName,
+      email: this.UserData.email,
+      phoneNumber: this.UserData.phoneNumber,
+    });
+
+    this.UserData.userInGroups.forEach((element, index) => {
+      this.adduserInGroup()
+      this.userInGroupsList.controls[index].patchValue({
+
+
+      });
+
+      console.log("🚀  element", element)
+
+    });
+      }
+
   get f() {
     return this.TenantUserform.controls
   }
+  get userInGroupsList(): FormArray {
+    return this.TenantUserform.controls['userInGroups'] as FormArray;
+}
+
+
 }
--- src/modules/tenant/data/TenantUser.ts
+++ src/modules/tenant/data/TenantUser.ts
@@ -11,3 +11,35 @@ export interface TenantUserList {
     password: string;
     tenantList:string[]
   }
+  export interface UserData {
+    userId: string
+    userName: string
+    email: string
+    phoneNumber: any
+    isActive: boolean
+    userInGroups: UserInGroup[]
+  }
+  
+  export interface UserInGroup {
+    tenantUserId: number
+    timeAdded: string
+    timeRemoved: any
+    groupAdmin: boolean
+    tenantId: string
+    userTenant: UserTenant
+    isActive: boolean
+  }
+  
+  export interface UserTenant {
+    id: string
+    name: string
+    databaseName: string
+    tenantGroupTypeId: number
+    insertTs: string
+    userTenantGroupType: UserTenantGroupType
+  }
+  
+  export interface UserTenantGroupType {
+    id: number
+    typeName: string
+  }
